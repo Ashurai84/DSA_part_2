1df418f0a866080830af9ee8f3a2d9d3f4274e3e
Ashurai84 <raia40094@gmail.com>
2025-07-28 10:30:20 +0530

    this is addition of prefix

---
+++ MainExam.cpp
@@ -0,0 +1,21 @@
+// // Prefix Sum Implemnetion 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> arr = {10, 20, 30, 40, 30};
+    
+    // Loop to compute prefix sums
+    for (int i = 1; i < arr.size(); i++) {
+        arr[i] = arr[i] + arr[i - 1];
+    }
+
+    // Print the prefix sum array
+    for (int num : arr) {
+        cout << num << " ";
+    }
+
+    return 0;
+}
